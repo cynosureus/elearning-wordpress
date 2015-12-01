01213614b10a49d5c5baf0d62135aa854dab4408
Dan Miller <DanMiller@adminisatorsmbp.cynosurelaser.com>
2015-12-01 16:31:29 -0500

    refactored for marketing materials

--- functions.php
+++ functions.php
@@ -129,6 +129,16 @@ function cynosure_elearning_widgets_init() {
 		'after_title'   => '</h2>',
 	) );
 
+	register_sidebar( array(
+		'name'          => esc_html__( 'Marketing Sidebar', 'cynosure_elearning' ),
+		'id'            => 'elearning-marketing-sidebar',
+		'description'   => '',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
+
 
 }
 add_action( 'widgets_init', 'cynosure_elearning_widgets_init' );
